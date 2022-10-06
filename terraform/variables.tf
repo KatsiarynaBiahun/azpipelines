a4d34707a2c6b45d6ddc24090aea52bf0709495f
Katariina-Biahun <70053245+KatsiarynaBiahun@users.noreply.github.com>
2022-10-06 12:06:41 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "myKBResourceG"
+  default = "mykbrgkatkat"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "myKBAppName"
+  default     = "mykbappkatkat"
 }
 
 variable "location" {

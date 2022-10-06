646f4bde9405ea8fce672d1332ba024fb69236b2
Katariina-Biahun <70053245+KatsiarynaBiahun@users.noreply.github.com>
2022-10-06 12:23:42 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,11 @@
 variable "resource_group" {
-  description = "The resource group"
   default = "mykbrgkatkat"
 }
 
 variable "application_name" {
-  description = "The Spring Boot application name"
   default     = "mykbappkatkat"
 }
 
 variable "location" {
-  description = "The Azure location where all resources in this example should be created"
   default     = "westeurope"
 }
